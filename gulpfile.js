2f03ecd26289e742c7c35f6200ab738f6defb6aa
Andreas Bratfisch <duskman72@gmail.com>
2015-12-11 21:09:04 +0100

    modified gulpfile.js: show error message

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var notify = require('gulp-notify');
 
+logError = function(err) {
+    //console.log(err.message);
+    notify({message:err.message,title:"Error",wait:true});
+}
+
 gulp.task('default', function () {  
-    gulp.src('./app/resources/scss/*.scss')
-    .pipe(sass({
-        includePaths: ['./app/resources/scss'],
-		style: 'expanded'
-    }))
+    gulp.src('./app/resources/scss/app.scss')
+    .pipe(sass().on('error', notify.onError(function(error){
+        return {message:error.message,title:"Error running SASS"};
+    })))
     .pipe(gulp.dest('public/css'))
-	.pipe(notify({message:"message",title:"GULP TITLE"}))
+	.pipe(notify({title:"Running SASS", message:"finished"}))
 });
