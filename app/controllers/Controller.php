436476332bbd365d0cb726d562088c45914d8e2d
Andreas Bratfisch <duskman72@gmail.com>
2015-12-12 10:08:21 +0100

    modified .gitignore, added app.scss

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -8,10 +8,24 @@ use Simplified\Http\Response;
 
 class Controller {
     public function newIndex(Request $req, $arg1, $arg2) {
+
+        print '
+        <html>
+        <head>
+            <link rel="stylesheet" href="/css/app.css"/>
+        </head>
+        <body>
+            <a href="#" class="btn btn-success">Test Button</a>
+        </body>
+        </html>
+        ';
+
+        /*
         $content =  Lang::get('globals.hello1', array('name' => 'ich'));
 
         $response = new Response();
         $response->setContent($content);
         $response->send();
+        */
     }
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,43 @@
 var gulp = require('gulp');  
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 var notify = require('gulp-notify');
 
-logError = function(err) {
-    //console.log(err.message);
-    notify({message:err.message,title:"Error",wait:true});
-}
+var config = new Object();
+config.basepath = "app/resources/";
 
-gulp.task('default', function () {  
-    gulp.src('./app/resources/scss/app.scss')
-    .pipe(sass().on('error', notify.onError(function(error){
-        return {message:error.message,title:"Error running SASS"};
-    })))
-    .pipe(gulp.dest('public/css'))
-	.pipe(notify({title:"Running SASS", message:"finished"}))
+var simplified = new function() {
+    this.sass = function(file) {
+        var path = config.basepath + file;
+        gulp.src(path)
+            .pipe(sass({includePaths:config.basepath + "vendor/*", style:"expanded"})
+                .on('error',
+                    notify.onError(function(error){
+                        return {message:error.message,title:"Error running SASS"};
+                    })
+                )
+            )
+            .pipe(gulp.dest('public/css'))
+        return this;
+    };
+
+    this.copy = function(src, dst) {
+        gulp.src(config.basepath + src).pipe(gulp.dest(dst));
+        return this;
+    };
+};
+
+gulp.task('simplified', function(){
+    return simplified.sass("scss/app.scss")
+        .copy('vendor/jquery/jquery.min.js', 'public/js');
+});
+
+gulp.task('watch', function(){
+    watch(config.basepath+"scss/*.scss").on('change', function(file){
+        gulp.start('simplified');
+    });
+});
+
+gulp.task('default', function () {
+    gulp.start('simplified');
 });
