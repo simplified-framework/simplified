90360c59ff89a251e7b8d02fe5ce375ae0c786ac
Andreas Bratfisch <duskman72@gmail.com>
2015-12-13 14:59:05 +0100

    start creating a useful plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,8 @@
-var gulp = require('gulp');  
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
-var notify = require('gulp-notify');
-
-/*
-var simplified = new function() {
-    this.sass = function(file) {
-    }
-
-    this.copy = function(src, dst) {
-    }
-}
-
-var elixir = function(fn) {
-     fn(simplified);
-};
+var elixir = require('./elixir');
 
 elixir(function(mix) {
     mix
-        .sass("scss/app.scss")
-        .copy('vendor/jquery/jquery.min.js', 'public/js')
-        .copy('vendor/jquery/jquery.min.js', 'public/js')
-        .copy('vendor/jquery/jquery.min.js', 'public/js');
-});
-*/
-
-gulp.task('scripts', function(){
-    gulp.src('app/resources/vendor/jquery/jquery.min.js')
-        .pipe(gulp.dest('public/js'));
-});
-
-gulp.task('sass', function(){
-    gulp.src('./app/resources/scss/**/*.scss')
-        .pipe(sass({includePath:'./app/resources/vendor'}).on('error', notify.onError(function(err){
-            return {title:'Simplified Build',message:err.message,icon:__dirname + '/icons/error.png'};
-        })))
-        .pipe(gulp.dest('public/css'))
-        .pipe(notify({title:'Simplified Build',message:'Build successfully finished',icon:__dirname + '/icons/finished.png'}));
-});
-
-gulp.task('default', function(){
-    gulp.start('sass','scripts');
-});
-
-gulp.task('watch', function () {
-    gulp.watch('./app/resources/scss/**/*.scss', ['sass']);
+        .sass("app/resources/assets/scss/**/*.scss")
+        .copy('app/resources/vendor/jquery/jquery.min.js', 'public/js')
+        .copy('app/resources/vendor/bootstrap-sass/assets/fonts/bootstrap/*', 'public/fonts')
 });
