620c385306f45615499ecc1380ef4f5f3238f8c9
Andreas Bratfisch <duskman72@gmail.com>
2015-12-16 07:13:39 +0100

    implement twig view renderer

--- gulpfile.js
+++ gulpfile.js
@@ -5,4 +5,5 @@ elixir(function(mix) {
         .sass("assets/scss/**/*.scss")
         .copy('vendor/jquery/jquery.min.js', 'public/js')
         .copy('vendor/bootstrap-sass/assets/fonts/bootstrap/*', 'public/fonts')
+        .copy('vendor/bootstrap-sass/assets/javascripts/bootstrap.min.js', 'public/js')
 });
