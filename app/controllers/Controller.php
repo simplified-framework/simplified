5c90cbef4acbc651c316f708fdc4696cdc150b0b
Andreas Bratfisch <duskman72@gmail.com>
2015-12-18 07:44:35 +0100

    test new code

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -10,14 +10,8 @@ use Simplified\Http\Response;
 class Controller {
     public function newIndex(Request $req, $arg1, $arg2) {
         $v = new View();
-        return $v->render('index.twig');
+        $content = $v->render('index.twig');
 
-        /*
-        $content =  Lang::get('globals.hello1', array('name' => 'ich'));
-
-        $response = new Response();
-        $response->setContent($content);
-        $response->send();
-        */
+        (new Response($content, 200))->send();
     }
 }
