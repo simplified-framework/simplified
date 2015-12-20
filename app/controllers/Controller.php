6536be562521be51a44121ddb99c7fca62f1e65a
Andreas Bratfisch <duskman72@gmail.com>
2015-12-20 19:54:06 +0100

    test new dbal and orm module

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -2,16 +2,20 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
 use Simplified\Core\View;
 use Simplified\Http\Response;
 
 class Controller {
     public function newIndex() {
-
+        $user = new User();
+        var_dump($user->getTable()->fieldNames());
+        /*
         $options = array("opt1" => "Option 1", "opt2" => "Option 2");
 
         $v = new View();
         $content = $v->render('index.twig', compact('options'));
         (new Response($content))->send();
+        */
     }
 }
