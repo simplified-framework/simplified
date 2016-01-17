504ff3fa360626f9da4dd1f40e909d13d59fa183
Andreas Bratfisch <duskman72@gmail.com>
2016-01-17 17:12:49 +0100

    fix package.json,gulfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
 var build = require('simplified-build');
 
 build(function(mix) {
-    mix
-		var jqueryPath = 'vendor/jquery';
-		var jqueryuiPath = 'vendor/jquery-ui';
-		var bootboxPath = 'vendor/bootbox';
-		var tinymcePath = 'vendor/tinymce';
-		var hintcssPath = 'vendor/hint.css';
-		var fontawesomePath = 'vendor/font-awesome';
-		var tagsPath = 'vendor/tag-it';
-		var opensansFontPath = 'vendor/google-open-sans';
-		var underscorestringPath = 'vendor/underscore.string';
-		
-		mix.sass('scss/*')
-			.copy(jqueryPath + '/jquery.min.js', 'public/scripts')
-			.copy(jqueryuiPath + '/jquery-ui.min.js', 'public/scripts')
-			.copy(bootboxPath + '/bootbox.js', 'public/scripts')
-			.copy(tinymcePath, 'public/scripts/tinymce')
-			.copy(underscorestringPath + '/dist/underscore.string.min.js', 'public/scripts')
-			.copy(tagsPath + '/js/tag-it.min.js', 'public/scripts')
-			.copy(tagsPath + '/css/jquery.tagit.css','public/css')
-			.copy(fontawesomePath + "/css", 'public/css')
-			.copy(hintcssPath + "/hint.min.css", 'public/css')
-			.copy(fontawesomePath + "/fonts", 'public/fonts')
-			.copy(opensansFontPath + '/open-sans', 'public/fonts/open-sans')
-			.copy(bpath + '/fonts', 'public/fonts')
-			.copy(bpath + '/js/bootstrap.min.js', 'public/scripts');
+	var bpath = 'vendor/bootstrap-sass/assets';
+	var jqueryPath = 'vendor/jquery';
+	var jqueryuiPath = 'vendor/jquery-ui';
+	var bootboxPath = 'vendor/bootbox';
+	var tinymcePath = 'vendor/tinymce';
+	var hintcssPath = 'vendor/hint.css';
+	var fontawesomePath = 'vendor/font-awesome';
+	var tagsPath = 'vendor/tag-it';
+	var opensansFontPath = 'vendor/google-open-sans';
+	var underscorestringPath = 'vendor/underscore.string';
+	
+	mix.sass('scss/*')
+		.copy(jqueryPath + '/jquery.min.js', 'public/scripts')
+		.copy(jqueryuiPath + '/jquery-ui.min.js', 'public/scripts')
+		.copy(bootboxPath + '/bootbox.js', 'public/scripts')
+		.copy(tinymcePath, 'public/scripts/tinymce')
+		.copy(underscorestringPath + '/dist/underscore.string.min.js', 'public/scripts')
+		.copy(tagsPath + '/js/tag-it.min.js', 'public/scripts')
+		.copy(tagsPath + '/css/jquery.tagit.css','public/css')
+		.copy(fontawesomePath + "/css", 'public/css')
+		.copy(hintcssPath + "/hint.min.css", 'public/css')
+		.copy(fontawesomePath + "/fonts", 'public/fonts')
+		.copy(opensansFontPath + '/open-sans', 'public/fonts/open-sans')
+		.copy(bpath + '/fonts', 'public/fonts')
+		.copy(bpath + '/javascripts/bootstrap.min.js', 'public/scripts');
 });
